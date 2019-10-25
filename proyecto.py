97189781eb7ced88904420b8365e434799f77af7
FelixCabrera23 <56407420+FelixCabrera23@users.noreply.github.com>
2019-10-25 00:16:54 -0600

    Add files via upload

---
+++ proyecto.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 23:39:50 2019
+
+@author: Félix Cabrera
+USAC - ECFM
+Proyecto de Materia Condensada: PITO PITO PITO PITO ME GUSTA EL PITO 
+Particulas en un resipiente
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+"Variables importantes a lo largo del programa:"
+L = 100 # Longitud de la caja
+r = L/10 # Radio de las particulas 
+d = r*2 # Diametro de las particulas
+A = L*L # Area del recipiente
+xmin = r # Valor minimo de la coordenada x
+xmax = L -r # Valor maximo de la coordenada x
+ymin = xmin # Valor minimo de la coordenada y
+ymax = xmax # Valor maximo de la coordenada y
+a0 = np.pi/3# Angulo minimo de la red hexagonal
+
+"Calculo de N"
+nc =int(L/d)
+nf = int((L)/(d*np.sin(a0)))
+N = nc*nf -int(nf/2)
+
+def circulo(x,y):
+    "define el circulo en las coordenadas y con su radio apropiado"
+    circ = plt.Circle((x,y),r,fill=False)
+    return circ
+
+def Grafica(red):
+    "Esta parte plotea"
+    ax = plt.gca()
+    for i in range(len(red)):
+        ax.add_patch(red[i].grafica())
+    plt.axis([0,L,0,L])
+    plt.show()
+
+def xy(r,phi): 
+    "Esta funcion regresa las coordenadas de polares a cartesianas"
+    return r*np.cos(phi), r*np.sin(phi)
+    #Fuente: https://www.iteramos.com/pregunta/83886/trazar-un-circulo-con-pyplot
+
+class Particula(object):
+    """Esta clase define la particula con sus coordenadas en el plano x y
+    Sera necesario llamar al objeto por medio de una variable que tendra 
+    las caracteristicas que la definen que son sus coordenadas."""
+    
+    def __init__(self, x=0, y=0, n=0):
+        "Esto define las coordenadas x y tambien el numero de particulas que es"
+        self.x = x
+        self.y = y
+        self.n = n
+
+    def grafica(self):
+        "Esto le da a cada particula su grafica"
+        # Para llamarla usar Particula.grafica()
+        c = circulo(self.x,self.y)
+        return c
+        
+    def __repr__(self):
+        "Esto hace que al llamar la variable nos devuelva el nombre del objeto"
+        return 'Particula{0.n!r}({0.x!r},{0.y!r})'.format(self)
+    
+    # Hace falta delimitar que los valores x y solo sean numericos pero que weba
+    
+# Ya comprobe que puedo guardar las particulas en una lista
+# Ya comprobe que puedo sacar valores de x y de las listas
+# Al llenar la lista entonces tenemos las particulas ya indexadas con el orden de la lista    
+" Esto es la red hexagonal"
+ph0 = Particula(xmin,ymin,0) # Primer particula de la red hexagonal
+redhex = [ph0] # Lista de las particulas en la red hexagonal
+
+for i in range(N-1):
+    "Esta parte leera las coordenadas de la particula anterior"
+    xi = redhex[i].x
+    yi = redhex[i].y
+    xn = xi + d
+    yn = yi
+    n = i +1
+    if int(xn) == int(xmax+r):
+        xn = xmin
+        yn = yi + d*np.sin(np.pi/3)
+    elif xn > xmax+0.01:
+        xn = xmin + r
+        yn = yi + d*np.sin(np.pi/3)
+    else:
+        xn = xn
+        yn = yn
+    phn = Particula(xn,yn,n)
+    redhex.append(phn)
+    
+
+def Quitar (p,red):
+    "Esta funcion quita las particulas aleatoreamente, quita p porciento de particulas de la red"
+    Ni = p/100
+    redhexn = red[:] #Copia de la lizta hexagonal que se va a limpiar
+    
+    for i in range(int(Ni*N)):
+        num = int(random.random()*len(redhexn))
+        redhexn.pop(num)
+        
+    "Es necesario re indexsarlas en la nueva lista"
+    for i in range(len(redhexn)):
+        redhexn[i] = Particula(redhexn[i].x,redhexn[i].y,i)
+        
+    return redhexn
+
+
+def Mover (P):
+    "Esta parte mueve las particulas aleatoreamente sin que se salgan de los limites"
+    ang = random.random()*2*np.pi
+    xn = P.x + d*np.cos(ang)
+    yn = P.y + d*np.sin(ang)
+    if xn <= xmax and xn >= xmin and yn <= ymax and yn >= ymin:
+        P = Particula(xn,yn,P.n)
+    else:
+        P = P
+    return P
+
+
+            
+
+
+
+
+
+    
+
+    
+
+    
+    
+    
+    
+
+    
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
