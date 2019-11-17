166ddcc2dec6f474e7feb1cf8d29708e7c8cadc0
FelixCabrera23 <56407420+FelixCabrera23@users.noreply.github.com>
2019-11-16 21:26:53 -0600

    Añadio london

--- Coloidepy3.py
+++ Coloidepy3.py
@@ -7,44 +7,148 @@ Created on Fri Oct 11 23:39:50 2019
 USAC - ECFM
 Proyecto de Materia Condensada: 
 Particulas en un recipiente
+
+Este programa consta de varias partes que funciónan por separado y puede ser
+modificado facilmente para lograr analizar distintas configuraciónes de particulas
+asi como utilizando los potenciales de Lenard Jones como el potencial de London.
+
+El programa funcióna a partir de una lista de objetos llamados Particulas. Estas tienen propiedades, 
+tienen un indice, que indica su posición dentro de una lista, este es unico para cada particula
+guardan su posición en el plano xy tambien guardan su "energia", esta es la energia para añadir esta
+particula al sistema en ultimo lugar. 
+
+Este programa presenta los datos de forma grafica y tambien los almacena en documentos de texto plano.
+El programa empieza llenando una lista con las particulas necesarias para llenar el espacio
+determinado por las constantes que aparecen en la primera seccion de codigo. Modificando
+estas variables puede cambiar el tamaño del recipiente, y de las particulas, por lo tanto 
+tambien modificara el numero de particulas maximas en el recipiente.
+
+NOTA: debido a limitaciones de software el programa no puede lidear con tamaños muy pequeños
+para las dimensiones tanto de la caja como de las particulas, por lo tanto se utilizan a escala. 
+En la presente configuración la escala es 1 = 1 x 10 ^-6 metros.
+
+La red con el llenado maximo se denomina redhex
+
+INSTRUCCIÓNES:
+    GENERAR PORCENTAJES INICIALES:
+    Para Generar una configuración de particulas aleatoreas, (un porcentaje de llenado) 
+    debe crear una lista donde se puedan almacenar. Luego utilice la función Quitar(p,red), donde 
+    p es el porcentaje de llenado y red es la red de donde desea partir.
+    Ejemplo:
+        red20 = Quitar(20,redhex)
+        
+        red20 = [Particula0(2.5,0.5), Particula1(3.5,0.5), Particula2(8.5,0.5), Particula3(16.5,0.5),...
+      
+        Esto le dara una red, almacenada en la lista red20 que tiene el 20 porciento de llenado. 
+        Se utiliza la redhex como madre ya que esta red presenta un llenado de 100.
+    
+    El segundo paso para trabajar con una configuración inicial sera guardarla. Para eso haremos uso
+    de la función Guardar_archivo(red,nombre). Esta función guardara una grafica de la red y
+    tambien generará un archivo de texto plano con las posiciones de las particulas en el plano xy
+    Ejemplo:
+        Guardar(red20,'ejemplo')
+        
+        Esto generara un archivo de texto con las posiciones y una grafica donde se podran observar la 
+        configuración del sistema en el espacio.
+        
+    RECUPERAR Y ABRIR ARCHIVOS DE CONFIGURACIONES:
+    Para trabajar con configuraciones guardadas previamente o con configuraciones de otras fuentes 
+    haremos uso de la función Recuperar(archivo). De nuevo, como en el caso de la función quitar,
+    esta regresa una lista de las particulas en las posiciones que se encuentren detalladas en el archivo
+    de texto. Para recuperar un archivo debe introducir el nombre exacto del archivo de origen, incluyendo
+    su extencion, es necesario introducirlo dentro de comillas.
+    Ejemplo:
+        
+        red_recuperarda = Recuperar('ejemplo.txt')
+        red_recuperada = [Particula0(2.5,0.5), Particula1(3.5,0.5), Particula2(8.5,0.5),...
+    
+    
+    REALIZAR SIMULACIÓNES DE TIPO MONTECARLO
+    
+    IMPORTANTE:
+        PARA QUE EL CODIGO CALCULE EL POTENCIAL DE LONDON DEBE ASIGNARSE MANUALMENTE 
+        EL VALOR 1 A LA VARIABLE "POTENCIAL", EL VALOR CERO SE UTILIZA PARA EL 
+        POTENCIAL DE L-J. Para cambiar entre el potencial de London atractivo y repulsivo 
+        basta con cambiar de signo a la constante A (linea 129).
+    
+    Montecarlo_1():
+    Existen tres tipos fundamentales de simulaicónes montecarlo que puede realizar este programa,
+    su uso dependera de las necesidades del usuario. La forma más simple para realizar un metodo de
+    montecarlo sera por medio de la función Montecarlo_1(pasos,porcentaje,'nombre')
+    Esta funcion empieza generando una configuracion inicial para el porcentaje que se ingrese "porcentaje"
+    como entrada. A partir de esta configuración inical realizara la cantidad n de "pasos" indicada,
+    debera ingresarse un nombre particular, entre comillas 'nombre' para que el programa 
+    pueda guardar los archivos necesarios del proceso. Al finalizar se generaran graficas de la
+    configuracion inicial y de la configuracion final y se guardarn en archivos de texto. otros
+    datos importantes, como el tiempo y la cantidad de pasos aceptados se desplegaran 
+    en pantalla.
+    
+    Montecarlo_2():
+    El segundo tipo de simulacion montecarlo empieza apartir de un archivo de texto plano con una
+    configuración inicial. Este puede ser simplemente una configuracion inicial o puede sera a partir
+    de una configuracion final que ya este procesada.
+    La funcion se debe escribir de la siguiente forma: Montecarlo_2(pasos,'nombre')
+    Entonces el programa buscara en la carpeta donde se encuentre al archivo 'nombre', por lo que debe
+    de incluir la extención. Sobre esta configuración inical realizara una opitmización de N pasos,
+    segun determine el usuario.
+    
+    Montecarlo_3():
+    El tercer tipo de simulación de montecarlo, es la más compleja, Esta funciona realizando corridas
+    de un numero fijo de pasos cada una. Al finalizar Guarda graficas y archivos de texto donde se observa
+    el analizis de los tiempos, la energia y la aceptación de pasos. Este es el metodo más recomendable
+    pára utilizar. Dentro del codigo tambien se encuentra un criterio de convergencia, por lo que se puede
+    proponer realizar un numero grande de corridas y si el sistema termaliza se detendra el proceso
+    automaticamente. Ejemplo:
+        Montecarlo_3(pasos, corridas, porcentaje)
+        
+    Al igual que el primer metodo este generara su propia configuración inicial, sobre el cual realizara
+    las corridas, cada una de la cantidad de pasos indicados.
+    
+
 """
 
 import matplotlib.pyplot as plt
-import matplotlib
 import numpy as np
 import random
 from datetime import datetime
 from time import time
 import sys
 
+"Variable de desición de potencial, seleccióne 0 para L-J o 1 para London"
 
-"Variables importantes a lo largo del programa:"
+Potencial = int(1)
+
+"Dimensiones de la caja y de las particulas"
 L = 20 # Longitud de la caja
 r = 0.5# Radio de las particulas 
 d = r*2 # Diametro de las particulas
-A = L*L # Area del recipiente
+
+"Constantes de los potenciales"
+e = 1 #Profundidad del potencial
+omax = d*2 # distancia en el cual el potencial es cero
+A = r*1.5 # Constante de London
+
+"Variables importantes a lo largo del programa:"
 xmin = r # Valor minimo de la coordenada x
 xmax = L -r # Valor maximo de la coordenada x
 ymin = xmin # Valor minimo de la coordenada y
 ymax = xmax # Valor maximo de la coordenada y)):,
 a0 = np.pi/3.0# Angulo minimo de la red hexagonal
-e = 1 #Profundidad del potencial
-omax = d*2 # distancia en el cual el potencial es cero
-inicio = datetime.now()
-random.seed(1999)
-Emax = float(1)
-x = [] # Lista de las coordenadas de la red, ordenadas con los indices de las particulas
-y = []
+inicio = datetime.now() #Fecha actual
+random.seed(1999) # Semilla primordial del sistema, determina entre otras cosas el vaciado de la red hexagonal
+Emax = float(1) # variable para almacenar el valor de la energia del sistema
+x = [] # Lista ordenada de las coordenadas de la red, ordenadas con los indices de las particulas
+y = [] # Lista ordenada de las coordenadas de la red.
 red_t = [] # Esta sera una copia de la red que nos servira para trabajar con ella, sin modificar la original
-Ecolormax = float(1)
-Efin = float(1)
+Efin = float(1) #Variable para almacenar valores temporales de energia.
 
-"Calculo de N"
+"Calculo de N, para calcular el numero maximo de particulas"
 nc =int(L/d)
 nf = int((L-r)/(d*np.sin(a0)))
 N = nc*nf -int(nf/2)
 
 def progress(count, total, status=''):
+    " Barra de progreso, ayuda a determinar el porcentaje del proceso"
     bar_len = 60
     filled_len = int(round(bar_len * count / float(total)))
 
@@ -142,6 +246,17 @@ def Energia_LJ (P_o,P_ext):
     
     return(E1)
     
+"Potencial de London"
+def Energia_London(P_o,P_ext):
+    "Este es el potencial de London para un par de particulas"
+    xo = P_o.x
+    yo = P_o.y
+    xi = P_ext.x
+    yi = P_ext.y
+    rl = np.sqrt((xo - xi)**2 + (yo - yi)**2)
+    E2 = A/rl
+    return(E2)
+    
 def Energia_red(red):
     "Esto toma una red y calcula la energia total"
     "Tambien le asigna su energia propia a cada particula"
@@ -154,7 +269,10 @@ def Energia_red(red):
             if j == i:
                 continue
             else:
-                En = Energia_LJ(Po,red[j])
+                if Potencial == 0:
+                    En = Energia_LJ(Po,red[j])
+                else:
+                    En = Energia_London(Po,red[j])
                 Et = Et + En
         red[i] = Particula(Po.x,Po.y,i,Et)
         
@@ -170,7 +288,10 @@ def Energia_Particula(P,red):
         if i == P.n:
             continue
         else:
-            Ei =Energia_LJ(P,red[i])
+            if Potencial == 0:
+                Ei = Energia_LJ(P,red[i])
+            else:
+                Ei = Energia_London(P,red[i])
             E = E + Ei
     return(E)
     
@@ -254,56 +375,42 @@ def Optim (red,pasos):
     else:
         Eo = Emax
     Ac = 0    
-    Energias = [1,2,3]
-    Cv_count = 0
     i = 0
-    saltos = 0
+
     while i < pasos: 
         "Vamos a mover una particula aleaotrea"
         num = int(random.random()*len(red_t))
         Pmov = Mover_Espc(num)
         
         if Pmov == red_t[num]:
-            saltos += 1
             continue
         
         Epo = (red_t[num].E)/2.0
         Epm = (Pmov.E)/2.0
         Emov = Eo - Epo + Epm
-        Ni = len(red)
         
         if Emov < Eo:
             Eo = Emov
             red_t[num] = Pmov
             x[num] = Pmov.x
             y[num] = Pmov.y
-            Energias.append(Emov)
-            if i % Ni/4 == 0:
-                standev = np.std(Energias[-Ni:-1])
-                prom = np.average(Energias[-Ni:-1])
-                Cv = standev / prom   # Coeficiente de variacion
-                if abs(Cv) < 0.000001:
-                    Cv_count+=1
-                if Cv_count == int(Ni/6):
-                    print('\n El proceso encontro convergencia despues de ' +str(i) +' pasos')
-                    break
             Ac += 1
             i += 1
-#            if i % (pasos*0.2) == 0 :
-#                Guardar_archivo(red_t,'backup'+str(i))      
+            continue
+
         else:
             i += 1
-            Lamda = random.random()
-            Ca = Ac/i
+            Lamda = random.random()*(1/3)
+            Ca = (Ac/i)
             if Lamda > Ca:
                 Eo = Emov
                 red_t[num] = Pmov
                 x[num] = Pmov.x
                 y[num] = Pmov.y
-                Energias.append(Emov)
+                continue
             else:
                 continue
-        progress(i,pasos, status = 'Optimizando:')
+
     Emax = Eo
     Efin = Eo
     return(red_t)
@@ -331,7 +438,7 @@ def Guardar_archivo (red_s,nombre):
     
     return('Su archivo ha sido guardado con exito')
 
-def Montecarlo(h,porcentaje,nombre):
+def Montecarlo_1(h,porcentaje,nombre):
     """Esta funcion utiliza las funciones desarrolladas anteriormente y calcula la posicion optima de
     una red de N particulas con radio r y el porcentaje de estas definido por el usuario
     """
@@ -381,7 +488,7 @@ def Montecarlo_2(h,nombre):
     print(inicio)
     print('Bienvenido: \nCalculando Energia...')
     to = time()
-    Recuperar('datos.txt')
+    Recuperar(nombre)
     redor = red_t[:]
     tf = time() - to
     print('Energia inicial: '+str(Emax))
@@ -401,24 +508,29 @@ def Montecarlo_2(h,nombre):
 
 
 
-def Caracterizacion(h,corridas,porcentaje):
-    "Esta funcion caracteriza el sistema en una nueva pc"
-    print('Bienvenido \n se caracterizara el sistema para '+str(corridas)+ 'de '+str(h)+' pasos cada una.')
+def Montecarlo_3(h,corridas,porcentaje):
+    """Esta función realiza una aproximación por el metodo de montecarlo
+    En varias etapas. Al finalizar guardara automaticamente las graficas de la energia y los
+    tiempos. Guardara los archivos con los datos utilizados para generar estas graficas en un 
+    documento .txt llamado caracterización.
+    """
+    print('Bienvenido \n Se realizaran '+str(corridas)+ ' corridas, de '+str(h)+' pasos cada una. \n Sobre una configuración de '+str(porcentaje)+' porciento de particulas')
     tiempos = [0]
     Energias = []
     Pasos = [0]
     Pasos_ac = [0]
     t1 = time()
-    print('calculando energia inicial... ')
+    print('Calculando energia inicial... ')
     redop = Quitar(porcentaje,redhex)
+    Guardar_archivo(redop,str(porcentaje)+' porciento, configuración inicial')
     t2 = time() - t1
     print('Calculada en un tiempo de: '+str(t2)+'s')
     Energias.append(Emax)
     t3 = time()
-    counter = 0
+    Cv_count = 0
     i = 0
     while i < corridas:
-        print('corrida '+str(i)+'\n')
+
         redop = Optim(redop,h)
         t4 = time() - t3
         Energias.append(Efin)
@@ -427,16 +539,22 @@ def Caracterizacion(h,corridas,porcentaje):
         Pasos.append(pas)
         Pasos_ac.append(Ac)
         i +=1
-        if i % int(corridas*0.2) == 0:
+        if i % int(corridas*0.1) == 0:
             Guardar_archivo(redop,str(porcentaje)+' porciento_'+str(i))
             print('\n')
-        if Ac == 0:
-            counter +=1
-        if counter == 100:
-            break
-            print('Se encontro convergencia despues de '+str(i)+' corridas.')
+        Ni = int(corridas/4)
+        if i % Ni == 0:
+            standev = np.std(Energias[-Ni:-1])
+            prom = np.average(Energias[-Ni:-1])
+            Cv = standev / prom   # Coeficiente de variacion
+            if abs(Cv) < 0.001:
+                Cv_count+=1
+            if Cv_count == 3:
+                print('\n El proceso encontro convergencia despues de ' +str(i) +' corridas')
+                break
         else:
             continue
+        progress(i,corridas, status = 'Optimizando:')
         
     "Grafica de tiempo vs Energia"
     plt.clf()
@@ -484,7 +602,21 @@ def Caracterizacion(h,corridas,porcentaje):
         file2.write('%f %f %i %i\n' % (t,E,Pa,Pm))
     file2.close()
     return()
-#
-#Caracterizacion(44,100,5)
+    
+"Ejemplos"
+"Recomendación personal: usen la función Montecarlo_3"
+"Este calcula 10 millones de pasos para 20 porciento, para el potencial L-J"
+#Potencial = 0
+#Montecarlo_3(50000,200,20)
+
+"Este calcula 5 millones de pasos para 30 porciento, para el potencial de London"
+#Potencial = 1
+#Montecarlo_3(50000,100,30)
+
+"Este genera una red llena al 50 porciento y lo guarda en un archivo de texto"
+#red50 = Quitar(50,redhex)
+#Guardar_archivo(red50,'red al 50 porciento')
+
+
 
 
